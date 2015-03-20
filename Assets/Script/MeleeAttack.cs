421d8063061fb4c6e272dd2a6ad91a3f8a31a0a7
Gustav Hedin <penthero@gmail.com>
2015-03-20 15:42:53 +0100

    added a new weapon, the sword. Fixed so all weapons are handled in the weaponHandler

---
+++ Assets/Script/MeleeAttack.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour {
+
+
+	public int damage = 40;
+	public float attackDelay = 0.4f;
+	public WeaponHandler weaponHandler;
+	private bool enemyInRange;
+	private Collider enemy;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		enemyInRange = false;
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if (enemyInRange) {
+			enemyInRange = false;
+
+			//EnemyHealth enemyHealth = enemy.collider.GetComponent<EnemyHealth>();
+			CompleteProject.EnemyHealth enemyHealth = enemy.GetComponent <CompleteProject.EnemyHealth> ();
+
+			if(enemyHealth)
+				enemyHealth.TakeDamage(damage, new Vector3(0, 0, 0));
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		// If the entering collider is the player...
+		if(!enemyInRange  && weaponHandler.isAttacking && other.tag == "Enemy" && (timer >= attackDelay))
+		{
+			// ... the player is in range.
+			Debug.Log ("in Range");
+			enemyInRange = true;
+			enemy = other;
+			timer = 0;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+
+	}
+}
---
+++ Assets/WeaponHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponHandler : MonoBehaviour {
+
+	public GameObject[] Weapons;
+	public bool isAttacking = false;
+
+	private Animator[] anims;
+	private int currentWeapon = 1;
+
+	// Use this for initialization
+	void Start () {
+
+		anims = new Animator[Weapons.Length];
+		for (int i = 0; i < anims.Length; ++i)
+			if (Weapons[i])
+				anims [i] = Weapons [i].GetComponent<Animator> ();
+
+		for (int i = 0; i < anims.Length; ++i)
+			if (Weapons[i])
+				if (i == currentWeapon)
+					Weapons [i].SetActive (true);
+				else
+					Weapons [i].SetActive (false);
+
+	}
+
+	void ChangeWeapons(int weaponId) {
+		if (weaponId >= 0 && weaponId < Weapons.Length && Weapons[weaponId] && (weaponId != currentWeapon)) {
+			Weapons [weaponId].SetActive (true);
+			Weapons [currentWeapon].SetActive (false);
+			currentWeapon = weaponId;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			isAttacking = true;
+			anims [currentWeapon].ResetTrigger ("Idle");
+			anims [currentWeapon].SetTrigger ("Attack");
+		} else if (Input.GetMouseButtonUp (0)) {
+			isAttacking = false;
+			anims [currentWeapon].ResetTrigger ("Attack");
+			anims [currentWeapon].SetTrigger ("Idle");
+		} else {
+			int weaponId;
+			bool isNumeric = int.TryParse(Input.inputString, out weaponId);
+
+			if(isNumeric)
+				ChangeWeapons(weaponId);
+		}
+	}
+}
